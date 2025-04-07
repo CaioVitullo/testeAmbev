cd95605f7c701b037169848b22c737140924d493
Caio Luis Vitullo <cvitullo@ciandt.com>
2025-04-07 07:09:17 -0300

    [AMBEV-6] Creating Repository

---
+++ backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/SaleRepository.cs
@@ -0,0 +1,55 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class SaleRepository : ISaleRepository<SaleServiceDto>
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<SaleServiceDto> _sales;
+
+        public SaleRepository(DbContext context)
+        {
+            _context = context;
+            _sales = context.Set<SaleServiceDto>();
+        }
+
+        public async Task<SaleServiceDto> GetByIdAsync(int id)
+        {
+            return await _sales.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<SaleServiceDto>> GetAllAsync()
+        {
+            return await _sales.ToListAsync();
+        }
+
+        public async Task AddAsync(SaleServiceDto entity)
+        {
+            await _sales.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(SaleServiceDto entity)
+        {
+            _sales.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var sale = await GetByIdAsync(id);
+            if (sale != null)
+            {
+                _sales.Remove(sale);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public IQueryable<SaleServiceDto> Query()
+        {
+            return _sales.AsQueryable();
+        }
+    }
+}
---
+++ backend/src/Ambev.DeveloperEvaluation.ORM/SaleDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.ORM
+{
+    public class SaleDbContext : DbContext
+    {
+        public SaleDbContext(DbContextOptions<SaleDbContext> options) : base(options) { }
+
+        public DbSet<Sale> Sales { get; set; }
+    }
+}
